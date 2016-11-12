1bb086a5b8f7bff16ffb91cb7bb3b161c896c894
Vinayak Pawar <vinayakapawar@gmail.com>
2016-11-12 06:48:41 +0530

    Generic list criteria object ...

---
+++ src/main/java/com/krishagni/commons/repository/AbstractListCriteria.java
@@ -0,0 +1,40 @@
+package com.krishagni.commons.repository;
+
+public abstract class AbstractListCriteria<T> {
+	private static final int MAX_RESULTS = 100;
+
+	private int startAt;
+
+	private int maxResults = MAX_RESULTS;
+
+	private boolean includeStats;
+
+	public boolean includeStats() {
+		return includeStats;
+	}
+
+	public T includeStats(boolean includeStats) {
+		this.includeStats = includeStats;
+		return self();
+	}
+
+	public int startAt() {
+		return startAt < 0 ? 0 : startAt;
+	}
+
+	public T startAt(int startAt) {
+		this.startAt = startAt;
+		return self();
+	}
+
+	public int maxResults() {
+		return maxResults <= 0 ? MAX_RESULTS : maxResults;
+	}
+
+	public T maxResults(int maxResults) {
+		this.maxResults = maxResults;
+		return self();
+	}
+
+	public abstract T self();
+}
