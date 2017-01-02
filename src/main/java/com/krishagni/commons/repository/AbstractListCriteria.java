3ca0b56ed1903aa6c21c33d153d4db0e6cd96b0b
Vinayak Pawar <vinayakapawar@gmail.com>
2017-01-02 12:34:32 +0530

    Added search term and include stats variables to list criteria object.

--- src/main/java/com/krishagni/commons/repository/AbstractListCriteria.java
+++ src/main/java/com/krishagni/commons/repository/AbstractListCriteria.java
@@ -3,18 +3,20 @@ package com.krishagni.commons.repository;
 public abstract class AbstractListCriteria<T> {
 	private static final int MAX_RESULTS = 100;
 
+	private String searchTerm;
+
 	private int startAt;
 
 	private int maxResults = MAX_RESULTS;
 
 	private boolean includeStats;
 
-	public boolean includeStats() {
-		return includeStats;
+	public String searchTerm() {
+		return searchTerm;
 	}
 
-	public T includeStats(boolean includeStats) {
-		this.includeStats = includeStats;
+	public T searchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
 		return self();
 	}
 
@@ -35,6 +37,16 @@ public abstract class AbstractListCriteria<T> {
 		this.maxResults = maxResults;
 		return self();
 	}
+
+	public boolean includeStats() {
+		return includeStats;
+	}
+
+	public T includeStats(boolean includeStats) {
+		this.includeStats = includeStats;
+		return self();
+	}
+
 
 	public abstract T self();
 }
